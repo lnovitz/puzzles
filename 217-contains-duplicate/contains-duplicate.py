978ff763851504ce35c85e3bf19d9a5d1ed613e8
Lianna <32498202+lnovitz@users.noreply.github.com>
2024-04-16 16:25:52 -0700

    Time: 411 ms (80.52%) | Memory: 32 MB (65.43%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -3,13 +3,16 @@ class Solution:
         # pseudocode
         # verify problem
         # keep a dictionary, key as the num in nums, value: number of times you see key
-        counter_dict = dict()
-        for num in nums:
-            if num in counter_dict.keys():
-                return True
-            else:
-                counter_dict[num] = 1
-        return False
+        # counter_dict = dict()
+        # for num in nums:
+        #     if num in counter_dict.keys():
+        #         return True
+        #     else:
+        #         counter_dict[num] = 1
+        # return False
+        new_nums = set(nums)
+        return len(new_nums) != len(nums)
+            
             
             
         
