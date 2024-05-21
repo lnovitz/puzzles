5ddece1bc68952165ddfb2fdd8eb6cda232d49ba
Lianna <32498202+lnovitz@users.noreply.github.com>
2024-05-21 11:45:20 -0700

    Time: 75 ms (5.16%) | Memory: 17.8 MB (15.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,36 +1,14 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
         if len(s) != len(t):
             return False
         else:
-            # now we need to count each letter
-            scounter = dict()
-            for letter in s:
-                if letter in scounter.keys():
-                    scounter[letter] += 1
-                else:
-                    scounter[letter] = 1
-            tcounter = dict()
-            for letter in t:
-                if letter in tcounter.keys():
-                    tcounter[letter] += 1
-                else:
-                    tcounter[letter] = 1
-            if scounter == tcounter:
-                return True
-            else:
-                return False
-
-
-test = Solution()
-s1 = "anagram"
-t1 = "nagaram"
-s2 = "rat"
-t2 = "car"
-assert test.isAnagram(s=s1, t=t1) == True
-assert test.isAnagram(s=s2, t=t2) == False
+            if s:
+                for letter in sorted(list(s)):
+                    s_dict.update({letter: s_dict.get(letter, 0)+1})
+            if t:
+                for letter in sorted(list(t)):                    
+                    t_dict.update({letter: t_dict.get(letter, 0)+1})
+            return s_dict == t_dict
