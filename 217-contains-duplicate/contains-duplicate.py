4e0facbf060866b8f86b3db424d67bd05ab2a649
Lianna <32498202+lnovitz@users.noreply.github.com>
2024-05-21 11:44:13 -0700

    Time: 402 ms (94.25%) | Memory: 32 MB (65.50%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,17 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # pseudocode
-        # verify problem
-        # keep a dictionary, key as the num in nums, value: number of times you see key
-        # counter_dict = dict()
-        # for num in nums:
-        #     if num in counter_dict.keys():
-        #         return True
-        #     else:
-        #         counter_dict[num] = 1
-        # return False
-        new_nums = set(nums)
-        return len(new_nums) != len(nums)
+        unique_nums = set()
+        for num in nums:
+            if num in unique_nums:
+                return True
+            else:
+                unique_nums.add(num)
+        return False
             
             
             
