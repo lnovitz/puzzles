97e0fb79119e8c339861be3d892baa6c75c04e4b
Lianna <32498202+lnovitz@users.noreply.github.com>
2024-05-30 08:43:59 -0700

    Time: 87 ms (5.27%) | Memory: 22.3 MB (6.69%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        converted_s = [x.lower() for x in s if x.isalnum()]
+        for i in range(0, len(converted_s), 1):
+            j = len(converted_s) - 1 - i
+            print("equal", converted_s[i], converted_s[j])
+            if converted_s[i] == converted_s[j]:
+                continue
+            else:
+                return False
+
+        return True
+                
